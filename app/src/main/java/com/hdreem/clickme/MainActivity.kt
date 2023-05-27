bfda436deb9e875c512e9f78631aa7e945670812
reemhd <reemhde@gmail.com>
2023-05-27 11:25:44 +0100

    added new activity

--- app/src/main/java/com/hdreem/clickme/MainActivity.kt
+++ app/src/main/java/com/hdreem/clickme/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.hdreem.clickme
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val clickButton = findViewById<View>(R.id.clickButton)
+        clickButton.setOnClickListener {
+            val intent = Intent(this@MainActivity, Intro::class.java)
+            startActivity(intent)
+        }
     }
 }
